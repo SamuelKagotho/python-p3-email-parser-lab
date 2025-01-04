2e670bb4d64a5e36c88dd2f776a19f5fa6168085
samuelkagotho <samuelkagotho@gmail.com>
2025-01-04 15:26:19 +0430

    resolved commit

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        email_pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+        
+        emails = re.findall(email_pattern, self.email_string)
+
+        return emails[::-1]
